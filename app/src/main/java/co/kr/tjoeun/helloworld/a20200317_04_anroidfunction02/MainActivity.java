913af174bc94aa00174f7db02a0278bb6c924f08
alonso14ss <reiyan92@gmail.com>
2020-03-17 17:04:50 +0900

    Initial commit

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200317_04_anroidfunction02/MainActivity.java
@@ -0,0 +1,19 @@
+package co.kr.tjoeun.helloworld.a20200317_04_anroidfunction02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import co.kr.tjoeun.helloworld.a20200317_04_anroidfunction02.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
